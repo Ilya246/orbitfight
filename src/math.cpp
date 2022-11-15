1246250624527f4ffc35fd21636ff27b915ff7bb
Ilya246 <ilyukarno@gmail.com>
2022-11-15 14:00:42 +0400

    Revert "Revert "extremely WIP (not working) experimental polygonal collisions""
    This reverts commit 5a8f6c4e6e4c85e7b8ee8727d8b107711fb6cbf7.

--- src/math.cpp
+++ src/math.cpp
@@ -24,4 +24,24 @@ float lerpRotation(float a, float b, float c) {
 	return a + c * deltaAngle(a, b);
 }
 
+bool AABB_collides(double x1, double y1, double sx1, double sy1, double x2, double y2, double sx2, double sy2) {
+	return x1 + sx1 > x2 && y1 + sy1 > y2 && x1 < x2 + sx2 && y1 < y2 + sy2;
+}
+double linesCollideX(double x11, double y11, double x21, double y21, double x12, double y12, double x22, double y22) {
+	double collx = x12 - x11 + (y12 - y11) / ((y21 - y11) / (x21 - x11) - (y22 - y12) / (x22 - x12));
+	return (collx > x11 && collx < x21 && collx > x12 && collx < x22) ? collx : INFINITY;
+}
+bool pointInAABB(double x, double y, double x1, double y1, double sx, double sy) {
+	return x < x1 + sx && y < y1 + sy && x > x1 && y > y1;
+}
+bool AABB_collideLine(double xr, double yr, double sx, double sy, double x1, double y1, double x2, double y2) {
+	if (pointInAABB(x1, y1, xr, yr, sx, sy) || pointInAABB(x2, y2, xr, yr, sx, sy)) {
+		return true;
+	}
+	double slope = (y2 - y1) / (x2 - x1);
+	double xi1 = x1 + (yr - y1) / slope, xi2 = x1 + (yr + sy - y1) / slope;
+	double yi1 = y1 + (xr - x1) * slope, yi2 = y1 + (xr + sx - x1) * slope;
+	return (xi1 > xr && xi1 < xr + sx) || (xi2 > xr && xi2 < xr + sx) || (yi1 > yr && yi1 < yr + sy) || (yi2 > yr && yi2 < yr + sy);
+}
+
 }
